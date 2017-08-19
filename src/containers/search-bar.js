32bf78c63d9e9de2e0363c618955bd18bf62173f
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-19 17:08:37 -0400

    preventing form to POST req

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            term: ''
+        };
+
+        //Binding this Searchbar
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    //METHODS
+    onInputChange(event) {
+        this.setState({ term: event.target.value })
+    }
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        //Fetch weather Data
+    }
+
     render(){
         return(
-            <form className="input-group">
-                <input />
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input
+                    placeholder="Get a 5 day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
+
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
